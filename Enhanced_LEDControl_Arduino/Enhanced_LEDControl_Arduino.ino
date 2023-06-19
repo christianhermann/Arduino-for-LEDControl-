2c5b73902cf27b017aed184b9c336f508e1e7633
Christian Hermann <christian.hermann94@alice.de>
2023-06-19 15:42:47 +0200

    Added FRETwPulseSupport

--- Enhanced_LEDControl_Arduino/Enhanced_LEDControl_Arduino.ino
+++ Enhanced_LEDControl_Arduino/Enhanced_LEDControl_Arduino.ino
@@ -137,6 +137,39 @@ void readProgramDataAndStart(uint8_t programNumber) {
       complexLight(LEDnumber, selectedLEDs, LEDs, pwmVal, tOn, tPause,tPauseAll, repeats);
       break;
     }
+    case 4: {//FRETwPulse
+Serial.println(F("Program 4: FRETwPulse selected. Waiting for data..."));
+      LEDnumber = readSerialDataInt8();
+
+      tOn = new unsigned long[LEDnumber];
+      pwmVal = new uint8_t[LEDnumber];
+      selectedLEDs = new uint8_t[LEDnumber];
+
+      for (int i = 0; i < LEDnumber; i++) {
+        selectedLEDs[i] = readSerialDataInt8();
+        pwmVal[i] = readSerialDataInt8();
+        tOn[i] = readSerialDataLong();
+        delay(100);
+      }
+      
+      tPause = readSerialDataLong();
+      
+
+      uint8_t selectedPulseLED = readSerialDataInt8();
+      uint8_t pulseLEDpwm = readSerialDataInt8();
+      unsigned long tPulseDur = readSerialDataLong();
+      unsigned long tPulseTime = readSerialDataLong();
+
+  // Serial.println(tPulseDur);
+  // Serial.println(tPulseTime);
+  // Serial.println(selectedPulseLED);
+  // Serial.println(pulseLEDpwm);
+
+  
+      Serial.println(F("Data Received, Starting FRETwPulse"));
+      FRETwPulse(LEDnumber, selectedLEDs, LEDs, pwmVal, tOn, tPause, tPulseDur, tPulseTime, selectedPulseLED, pulseLEDpwm);
+      break;
+    }
     default:
       Serial.println(F("ERROR - Program not found!- Restarting now"));
 Serial.flush();
